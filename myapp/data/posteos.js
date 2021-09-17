38ab48399d8265b073126417fcb0e59c6a9894e2
camicllanos <cllanos@udesa.edu.ar>
2021-09-16 21:01:49 -0300

    agrego sql y campos de data

--- myapp/data/posteos.js
+++ myapp/data/posteos.js
@@ -7,55 +7,55 @@ let posteos = {
                 fechaDeCreacion: "2021-01-02 09:11:15"
             },
             {
-                idUsuarioQueLoCreo: "2",
+                idUsuarioQueLoCreo: "1",
                 campoTextoDescriptivo: "Vayan ya a comprar mi nuevo libro que tanto tiempo dedique",
                 campoImagen: "",
                 fechaDeCreacion: "2021-02-03 15:24:40"          
             },
             {
-                idUsuarioQueLoCreo: "3",
+                idUsuarioQueLoCreo: "2",
                 campoTextoDescriptivo: "Amaneceres en Bariloche!. Hace mucho tiempo queria conocer esta maravilla de lugar y conocer sus platos tipicos",
                 campoImagen: "",
                 fechaDeCreacion: "2021-03-04 07:34:02"        
             },
             {
-                idUsuarioQueLoCreo: "4",
+                idUsuarioQueLoCreo: "2",
                 campoTextoDescriptivo: "y vos que esperas?, ya pediste nuestras nuevas hamburguesas?",
                 campoImagen: "",
                 fechaDeCreacion: "2020-12-8 22:13:56"
             },
             {
-                idUsuarioQueLoCreo: "5",
+                idUsuarioQueLoCreo: "3",
                 campoTextoDescriptivo: "preparando la comida junto a mi familia",
                 campoImagen: "",
                 fechaDeCreacion: "2021-03-04 07:34:02"
             },
             {
-                idUsuarioQueLoCreo: "6",
+                idUsuarioQueLoCreo: "3",
                 campoTextoDescriptivo: "cocinando para toda la comunidad",
                 campoImagen: "",
                 fechaDeCreacion: "2021-10-9 12:34:34"
             },
             {
-                idUsuarioQueLoCreo: "7",
+                idUsuarioQueLoCreo: "4",
                 campoTextoDescriptivo: "plan fit para el verano, rutinas incluidas y muchas mas recetas",
                 campoImagen: "",
                 fechaDeCreacion: "2020-06-12 16:00:45"
             },
             {
-                idUsuarioQueLoCreo: "8",
+                idUsuarioQueLoCreo: "4",
                 campoTextoDescriptivo: "estudiando y siempre con ganas de algo dulce!",
                 campoImagen: "",
                 fechaDeCreacion: "2020-09-11 10:55:23"
             },
             {
-                idUsuarioQueLoCreo: "9",
+                idUsuarioQueLoCreo: "5",
                 campoTextoDescriptivo: "miren las galletitas que me regalaron para mi cumpleanos",
                 campoImagen: "",
                 fechaDeCreacion: "2019-06-12 15:02:24"
             },
             {
-                idUsuarioQueLoCreo: "10",
+                idUsuarioQueLoCreo: "5",
                 campoTextoDescriptivo: "Abrimos nuevo local en San Fernando!! quien viene?",
                 campoImagen: "",
                 fechaDeCreacion: "2021-13-05 18:00:59"
