218c2cacf82ef07da27f2ea9a4e4dc0af48831af
Agostina-Maldonado <you@example.com>
2021-09-24 21:27:02 -0300

    cambiando los datos de id que estaban en string a dato de tipo número

--- myapp/controllers/postController.js
+++ myapp/controllers/postController.js
@@ -4,11 +4,11 @@ const comentarios = require('../data/comentarios');
 
 const postController={
     detailPost: function (req,res,next){
-        let idPost = req.params.id;
-        let infoPosteo = posts.buscarPorId(idPost); //esta funcion me buscar la información de los posteos por id.
+        let idPost = req.params.id; // capturo el id que viaja en la url con req.params.id y lo meto en la variable idPost
+        let infoPosteo = posts.buscarPorId(idPost); //Le envio como párametro de la funcion idPost para que me busque la información de los posteos por id.
         let infoComentarios = comentarios.lista; 
         return res.render('detallePost', { 
-            detallePost: infoPosteo, 
+            detallePost: infoPosteo, //es la info que le envio a la vista 
             listaComentario : infoComentarios}) //le envio la info del posteo y los comentarios a la vista
     },
     agregarPost: function (req,res,next){
--- myapp/data/usuarios.js
+++ myapp/data/usuarios.js
@@ -1,58 +1,58 @@
 let usuarios = {
     lista: [
             {
-                id: "1",
+                id: 1,
                 email: "moxfulder@yahoo.com",
-                documento: "73148074",
+                documento: 73148074,
                 constraseña: "moxfulder",
                 fotoDePerfil: "/foto1.jpg",
                 nombre: "Moxy",
                 apellido: "Fulder",
-                edad: "26"
+                edad: 26
             },
             {
                 
-                id: "2",
+                id: 2,
                 email: "dcoppit@live.com",
-                documento: "82210447",
+                documento: 82210447,
                 constraseña: "dcoppit",
                 fotoDePerfil: "/foto9.jpg",
                 nombre: "Damian",
                 apellido: "Coppit",
-                edad: "32"            
+                edad: 32            
             },
             {
                 
-                id: "3",
+                id: 3,
                 email: "gonzalesmaria@yahoo.com.ar",
-                documento: "44993031",
+                documento: 44993031,
                 constraseña: "gonzalesmaria1",
                 fotoDePerfil: "/foto3.jpg",
                 nombre: "Maria",
                 apellido: "Gonzales",
-                edad: "18"            
+                edad: 18            
             },
             {
                 
-                id: "4",
+                id: 4,
                 email: "marianorodr@hotmail.com",
-                documento: "16745667",
+                documento: 16745667,
                 constraseña: "mariano1234",
                 fotoDePerfil: "/foto8.jpg",
                 nombre: "Mariano",
                 apellido: "Rodriguez",
-                edad: "34"
+                edad: 34
             },
             {
                 
-                id: "5",
+                id: 5,
                 email: "camilasenna@hotmail.com",
-                documento: "42993039",
+                documento: 42993039,
                 constraseña: "camilasen12",
                 fotoDePerfil: "/foto5.jpg",
                 nombre: "Camila",
                 apellido: "Senna",
-                edad: "18"
+                edad: 18
             },
         ]
     }
