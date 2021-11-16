adcbe299f9cd09f50006a6742cec4a47491a35fe
Your Name <you@example.com>
2021-11-16 18:29:54 -0300

    ordeno las rutas

--- database/models/posteos.js
+++ database/models/posteos.js
@@ -15,16 +15,19 @@ module.exports = function(sequelize, dataTypes){
         caption: {
             type: dataTypes.STRING
             },
-        fechaDeCreacion: {
-            type: dataTypes.DATE
-        },
+        createdAt:{
+            type: dataTypes.DATE,
+            },
+        updatedAt:{
+            type: dataTypes.DATE,
+            },
         imagen:{
             type: dataTypes.STRING
             }
         }
       
     let config = {
-        timestamps:false,
+        timestamps:true, //Si la tabla no tiene los campos created_at y updated_at
         underscored:false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase. 
         tableName: "Posteos"
     }
--- myapp/routes/usuarios.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const usuariosController = require('../controllers/usuariosController')
-
-/* GET users listing. */
-router.get('/findAll', usuariosController.findAll);
-
-module.exports = router;
