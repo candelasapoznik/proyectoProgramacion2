96c9ca6eda5a0834296cd62cda5266730f75fa4d
Your Name <you@example.com>
2021-11-18 10:55:53 -0300

    modelos

--- myapp/database/models/Comentario.js
+++ myapp/database/models/Comentario.js
@@ -1,36 +1,43 @@
 module.exports = function(sequelize, dataTypes){
-
-    //Definir un alias.
-    let alias = 'Usuarios'; //Con este alias sequelize va a identificar internamente al archivo de modelo.
+    //Definir un alias
+    let alias = "Comentario"; //Con este alias sequelize va a identificar internamente al archivo de modelo.
 
     //Describir la configuración de las columnas de la tabla
-    let columnas = {
-        id:{
+    let cols = {
+        idPostComentario: {
             autoIncrement: true,
             primaryKey: true,
             type: dataTypes.INTEGER,
-        },
-        comentario:{
-            type: dataTypes.STRING,
-        },
-        fecha_creacion:{
+            },
+        idUsuarioQueLoCreo: {
+            type: dataTypes.INTEGER
+            },
+        idPost: {
+            type: dataTypes.INTEGER
+            },
+        textoComentario: {
+            type: dataTypes.STRING
+            },
+        createdAt:{
             type: dataTypes.DATE,
-        },
-        usuario_id:{
-            type: dataTypes.INTEGER,
-        },
-        post_id:{
-            type: dataTypes.INTEGER,
-        },
+            },
+        updatedAt:{
+            type: dataTypes.DATE,
+            }
+        }
+
+    let config = {
+        timestamps:true,  //Si la tabla no tiene los campos created_at y updated_at
+        underscored:false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.    
+        tableName: "Comentarios"
     }
 
-    let configuracionDeLaTabla = {
-        tableName: 'comentarios', 
-        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
-        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
+    const Comentario = sequelize.define(alias, cols, config); 
+    Comentario.associate = function(models){
+        Comentario.belongsTo(models.Usuario,{
+            as: 'usuarios',
+            foreignKey: 'idUsuarioQueLoCreo'
+        })
+    return Comentario; 
     }
-
-   const Comentarios = sequelize.define(alias, columnas, configuracionDeLaTabla);
-
-   return Comentarios;
-}
+} 
--- myapp/database/models/Posteo.js
+++ myapp/database/models/Posteo.js
@@ -1,36 +1,47 @@
 module.exports = function(sequelize, dataTypes){
-
-    //Definir un alias.
-    let alias = 'Posteos'; //Con este alias sequelize va a identificar internamente al archivo de modelo.
+    //Definir un alias
+    let alias = "Posteo"; //Con este alias sequelize va a identificar internamente al archivo de modelo.
 
     //Describir la configuración de las columnas de la tabla
-    let columnas = {
-        id:{
+    let cols = {
+        id: {
             autoIncrement: true,
             primaryKey: true,
-            type: dataTypes.INTEGER,
-        },
-        imagen:{
-            type: dataTypes.STRING,
-        },
-        descrip:{
-            type: dataTypes.STRING,
-        },
-        fecha_creacion:{
+            type: dataTypes.INTEGER
+            },
+        idUsuarioQueLoCreo: {
+            type: dataTypes.INTEGER
+            },
+        caption: {
+            type: dataTypes.STRING
+            },
+        createdAt:{
             type: dataTypes.DATE,
-        },
-        usuario_id:{
-            type: dataTypes.INTEGER,
-        },
+            },
+        updatedAt:{
+            type: dataTypes.DATE,
+            },
+        imagen:{
+            type: dataTypes.STRING
+            }
+        }
+
+    let config = {
+        timestamps:true, //Si la tabla no tiene los campos created_at y updated_at
+        underscored:false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase. 
+        tableName: "Posteos"
     }
 
-    let configuracionDeLaTabla = {
-        tableName: 'posteos', 
-        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
-        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
+    const Posteo = sequelize.define(alias, cols, config); 
+    Posteo.associate = function(models){
+        Posteo.belongsTo(models.Usuario,{
+            as: 'Usuarios',
+            foreignKey: 'idUsuarioQueLoCreo'
+        }),
+        Posteo.hasMany(models.Comentario,{
+            as: 'Comentarios',
+            foreignKey: 'idUsuarioQueLoCreo'
+        })
     }
-
-   const Posteos = sequelize.define(alias, columnas, configuracionDeLaTabla);
-
-   return Posteos;
-}
+    return Posteo; 
+} 
