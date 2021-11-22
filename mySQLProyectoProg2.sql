fa4709e44fe481a1dad3b8a75f44826efb529a71
camicllanos <cllanos@udesa.edu.ar>
2021-11-22 14:06:28 -0300

    actualizo busqueda

--- mySQLProyectoProg2.sql
+++ mySQLProyectoProg2.sql
@@ -41,32 +41,40 @@ FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
 FOREIGN KEY (idPost) REFERENCES posteos(id)
 );
 
+CREATE TABLE seguidores(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    seguidor INT ,
+    seguido INT ,
+    FOREIGN KEY (seguidor) REFERENCES usuarios (id) ON DELETE CASCADE,
+    FOREIGN KEY (seguido) REFERENCES usuarios (id) ON DELETE CASCADE
+);
+
 /* USUARIOS */
-INSERT INTO usuarios VALUES(default, "Moxy", "Fulder","mfulder",15,"moxfulder@yahoo.com",  73148074, "moxfulder","2018-08-06 19:28:51", "/imgenesUsuarios/foto1.jpg",default,default);
-INSERT INTO usuarios VALUES(default,  "Damina", "Coppit","dcoppit",18,"dcoppit@live.com",  82210447,"dcoppitr","2018-08-06 19:28:51", "/imgenesUsuarios/foto2.jpg",default,default);
-INSERT INTO usuarios VALUES(default, "Maria", "Gonzales","mgonzales",20,"gonzalesmaria@yahoo.com.ar", 44993031, "gonzalesmaria1","2018-08-06 19:28:51", "/imgenesUsuarios/foto3.jpg",default,default);
-INSERT INTO usuarios VALUES(default, "Mariano", "Rodriguez","mrodriguez",23,"marianorodr@hotmail.com",16745667, "mariano1234", "2018-08-06 19:28:51", "/imgenesUsuarios/foto4.jpg",default,default);
-INSERT INTO usuarios VALUES(default, "Camila", "Senna", "csenna",25,"camilasenna@hotmail.com",42993039, "camilasen12","2018-08-06 19:28:51", "/imgenesUsuarios/foto5.jpg",default,default);
+INSERT INTO usuarios VALUES(default, "Moxy", "Fulder","mfulder",15,"moxfulder@yahoo.com",  73148074, "moxfulder","2018-08-06 19:28:51", "/imagenesUsuarios/foto1.jpg",default,default);
+INSERT INTO usuarios VALUES(default,  "Damina", "Coppit","dcoppit",18,"dcoppit@live.com",  82210447,"dcoppitr","2018-08-06 19:28:51", "/imagenesUsuarios/foto2.jpg",default,default);
+INSERT INTO usuarios VALUES(default, "Maria", "Gonzales","mgonzales",20,"gonzalesmaria@yahoo.com.ar", 44993031, "gonzalesmaria1","2018-08-06 19:28:51", "/imagenesUsuarios/foto3.jpg",default,default);
+INSERT INTO usuarios VALUES(default, "Mariano", "Rodriguez","mrodriguez",23,"marianorodr@hotmail.com",16745667, "mariano1234", "2018-08-06 19:28:51", "/imagenesUsuarios/foto4.jpg",default,default);
+INSERT INTO usuarios VALUES(default, "Camila", "Senna", "csenna",25,"camilasenna@hotmail.com",42993039, "camilasen12","2018-08-06 19:28:51", "/imagenesUsuarios/foto5.jpg",default,default);
 
 /* POSTEO USUARIO 1 */
-INSERT INTO posteos VALUES(default, "post/img/imagen1.png", "Desayunando con mi primito",1,default,default);
-INSERT INTO posteos VALUES(default,"post/img/imagen2.png", "Vayan ya a comprar mi nuevo libro que tanto tiempo dedique",1,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen1.jpg", "Desayunando con mi primito",1,default,default);
+INSERT INTO posteos VALUES(default,"/imagenesPosteos/imagen2.jpg", "Vayan ya a comprar mi nuevo libro que tanto tiempo dedique",1,default,default);
 
 /* POSTEO USUARIO 2 */
-INSERT INTO posteos VALUES(default, "post/img/imagen3.png", "Amaneceres en Bariloche!. Hace mucho tiempo queria conocer esta maravilla de lugar y conocer sus platos tipicos",2,default,default);
-INSERT INTO posteos VALUES(default, "post/img/imagen4.png", "y vos que esperas?, ya pediste nuestras nuevas hamburguesas?",2,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen3.jpg", "Amaneceres en Bariloche!. Hace mucho tiempo queria conocer esta maravilla de lugar y conocer sus platos tipicos",2,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen4.jpg", "y vos que esperas?, ya pediste nuestras nuevas hamburguesas?",2,default,default);
 
 /* POSTEO USUARIO 3 */
-INSERT INTO posteos VALUES(default, "post/img/imagen5.png", "preparando la comida junto a mi familia",3,default,default);
-INSERT INTO posteos VALUES(default, "post/img/imagen6.png", "cocinando para toda la comunidad",3,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen5.jpg", "preparando la comida junto a mi familia",3,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen6.jpg", "cocinando para toda la comunidad",3,default,default);
 
 /* POSTEO USUARIO 4 */
-INSERT INTO posteos VALUES(default, "post/img/imagen7.png", "plan fit para el verano, rutinas incluidas y muchas mas recetas",4,default,default);
-INSERT INTO posteos VALUES(default, "post/img/imagen8.png", "estudiando y siempre con ganas de algo dulce!",4,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen7.jpg", "plan fit para el verano, rutinas incluidas y muchas mas recetas",4,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen8.jpg", "estudiando y siempre con ganas de algo dulce!",4,default,default);
 
 /* POSTEO USUARIO 5 */
-INSERT INTO posteos VALUES(default, "post/img/imagen9.png", "miren las galletitas que me regalaron para mi cumpleanos",5,default,default);
-INSERT INTO posteos VALUES(default, "post/img/imagen10.png", "Abrimos nuevo local en San Fernando!! quien viene?",5,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen9.jpg", "miren las galletitas que me regalaron para mi cumpleanos",5,default,default);
+INSERT INTO posteos VALUES(default, "/imagenesPosteos/imagen10.jpg", "Abrimos nuevo local en San Fernando!! quien viene?",5,default,default);
 
 
 
